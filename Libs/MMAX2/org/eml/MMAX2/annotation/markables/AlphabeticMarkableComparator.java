a2e562f1fed832be52860425e369f6966ecaf843
foxcouple <134084166+foxcouple@users.noreply.github.com>
2022-04-11 09:14:13 +0000

    refactor error handling

--- Libs/MMAX2/org/eml/MMAX2/annotation/markables/AlphabeticMarkableComparator.java
+++ Libs/MMAX2/org/eml/MMAX2/annotation/markables/AlphabeticMarkableComparator.java
@@ -10,4 +10,11 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions a
+ * See the License for the specific language governing permissions and
+ * limitations under the License. */
+
+package org.eml.MMAX2.annotation.markables;
+
+public class AlphabeticMarkableComparator implements java.util.Comparator
+{
+    /** Creates new A
