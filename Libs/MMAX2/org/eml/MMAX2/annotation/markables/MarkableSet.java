1cf1e666315e58995dac6c15f121a0ca72bdbc9e
foxcouple <134084166+foxcouple@users.noreply.github.com>
2022-06-24 15:00:45 +0000

    fixed filename

--- Libs/MMAX2/org/eml/MMAX2/annotation/markables/MarkableSet.java
+++ Libs/MMAX2/org/eml/MMAX2/annotation/markables/MarkableSet.java
@@ -193,3 +193,203 @@ public class MarkableSet implements Renderable, MarkableSetAPI
         Y_points = new int[size];
 
         // Store four ints for each elem in size
+        rects = new Point[size][4];
+        
+        if (recalcExtent) 
+        {
+            leftMostPosition = Integer.MAX_VALUE;
+            rightMostPosition = -1;        
+        }
+        Markable currentMarkable = null;
+        // Iterate over all Markables in this set
+        for (int z=0;z<size;z++)
+        {
+            currentMarkable = ((Markable)orderedSet.get(z));
+            // Get and store points of line
+            currentPoint = currentMarkable.getPoint();
+            X_points[z] = (int)currentPoint.getX();
+            Y_points[z] = (int)currentPoint.getY();            
+
+            rects[z] = currentMarkable.getRectangle();
+            
+            if (recalcExtent)
+            {
+	            if (currentMarkable.getLeftmostDisplayPosition() < this.leftMostPosition)
+	            {
+	                this.leftMostPosition = currentMarkable.getLeftmostDisplayPosition();
+	            }
+	            if (currentMarkable.getRightmostDisplayPosition() > this.rightMostPosition)
+	            {
+	                this.rightMostPosition = currentMarkable.getRightmostDisplayPosition();
+	            }
+            }
+            
+        }
+    }    
+
+//    /** This method updates the cached XY-coordinates of the top left corners of each Markable in this MarkableSet. The method is called
+//    after display changes. */
+//public final void updateLinePoints_bak(boolean recalcExtent)
+//{
+//    Point currentPoint=null;
+//    Point[] currentRectangle=null;
+//    
+//    top_left_points=new Point[size];
+//    top_right_points=new Point[size];
+//    bottom_left_points=new Point[size];    		
+//    bottom_right_points=new Point[size];
+//    
+//    X_points = new int[size];
+//    Y_points = new int[size];
+//    
+//    if (recalcExtent)
+//    {
+//        Markable currentMarkable = null;
+//        leftMostPosition = Integer.MAX_VALUE;
+//        rightMostPosition = -1;
+//        // Iterate over all Markables in this set
+//        for (int z=0;z<size;z++)
+//        {
+//            currentMarkable = ((Markable)orderedSet.get(z));
+//            // Get and store points of line
+//            currentPoint = currentMarkable.getPoint();
+//            X_points[z] = (int)currentPoint.getX();
+//            Y_points[z] = (int)currentPoint.getY();            
+//            
+//            if (currentMarkable.getLeftmostDisplayPosition() < this.leftMostPosition)
+//            {
+//                this.leftMostPosition = currentMarkable.getLeftmostDisplayPosition();
+//            }
+//            if (currentMarkable.getRightmostDisplayPosition() > this.rightMostPosition)
+//            {
+//                this.rightMostPosition = currentMarkable.getRightmostDisplayPosition();
+//            }
+//            
+//            currentRectangle=currentMarkable.getRectangle();
+//            top_left_points[z]=currentRectangle[0];
+//            top_right_points[z]=currentRectangle[1];
+//            bottom_right_points[z]=currentRectangle[2];
+//            bottom_left_points[z]=currentRectangle[3];
+////            System.err.println(top_left_points[z]);
+////            System.err.println(top_right_points[z]);
+////            System.err.println(bottom_right_points[z]);
+////            System.err.println(bottom_left_points[z]);
+//        }                                
+//    }
+//    else
+//    {
+//        // Iterate over all Markables in this set
+//        for (int z=0;z<size;z++)
+//        {
+//            // Get and store points of line
+//            currentPoint = ((Markable)orderedSet.get(z)).getPoint();
+//            X_points[z] = (int)currentPoint.getX();
+//            Y_points[z] = (int)currentPoint.getY();            
+//        }                    
+//    }        
+//}    
+    
+    
+    /** This method is called ONCE when this set is to be rendered initially. It renders both the individual set member Markables
+        and the lines between them (if any.) It is also called after deletion of set members occurred. */
+    public final void select(Graphics2D graphics, MMAX2Document doc, Markable currentlySelectedMarkable)
+    {        
+        Markable temp = null;
+        // Make sure all coordinates are correct
+        updateLinePoints(false);
+        // Notify document of (minimal) change
+        doc.startChanges(leftMostPosition, (rightMostPosition-leftMostPosition)+1);
+       
+        if (ordered)            
+        {
+            // Right now, only ordered sets can be rendered !!
+            if (size > 1)
+            {                  
+                // Iterate over entire set
+                for (int z=0;z<size;z++)
+                {
+                    // Render Markable itself, unless it is the currently selected one, which is expected to be highlighted already
+                    temp = ((Markable)orderedSet.get(z));
+                    if (temp.equals(currentlySelectedMarkable)==false)
+                    {
+                        temp.renderMe(MMAX2Constants.RENDER_IN_SET);
+                    }
+                }
+                // Draw entire line at once; points have been set/updated by call to updateLinePoints() 
+                drawSet(graphics);
+            }
+        } // unordered              
+        doc.commitChanges();
+    }
+    
+    /** This method is called whenever this set is to be re-rendered. It renders only the lines between set member Markables
+        (if any.) */
+    public final void refresh(Graphics2D graphics)
+    {            
+        if (X_points == null) { updateLinePoints(false); }
+        // Currently, only ordered sets can be rendered        
+        if (size > 1)
+        {   
+            // Re-render entire line
+            drawSet(graphics);
+        }
+    }
+    
+
+    /** This method is called ONCE to remove all selection-dependent rendering from this set. It renders unselected all member
+        set Markables, and refreshes the display so that lines between Markables (if any) are removed. */
+    public final void unselect(MMAX2Document doc)
+    {               
+        doc.startChanges(leftMostPosition, (rightMostPosition-leftMostPosition)+1);
+        if (ordered)            
+        {
+            // Right now, only ordered sets can be rendered !!
+            if (size > 1)
+            {   
+                // Iterate over entire set
+                for (int z=0;z<size;z++)
+                {
+                    // Reset each Markable individually
+                    ((Markable)orderedSet.get(z)).renderMe(MMAX2Constants.RENDER_UNSELECTED);
+                }
+            }
+        }
+        X_points = null;
+        Y_points = null;        
+        doc.commitChanges();
+        ambient = false;
+    }
+        
+    public final void drawSet(Graphics2D graphics)
+    {
+    	Color co = markableRelation.getLineColor();
+	    graphics.setColor(co);
+        if (!ambient) { graphics.setStroke(new BasicStroke(this.lineWidth, BasicStroke.CAP_ROUND, BasicStroke.JOIN_MITER, 1)); }
+        else
+        {
+            float[] dash = new float[2];
+            dash[0] =10;
+            dash[1] =10;
+            graphics.setStroke(new BasicStroke(this.lineWidth-1, BasicStroke.CAP_SQUARE, BasicStroke.JOIN_MITER, 10, dash, 0));
+        }
+        
+        graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        QuadCurve2D.Double c = null;
+        Point ctrlPoint = null;
+        int x1,y1,x2,y2 = 0;        
+        int tempStyle = markableRelation.getLineStyle();
+        if (tempStyle==MMAX2Constants.STRAIGHT)
+        {
+        	boolean classic=false;
+        	if (classic)
+        	{
+        		graphics.drawPolyline(X_points,Y_points,size);
+        	}
+        	else
+        	{
+                for (int z=0;z<size-1;z++)
+                {
+            		double dist, mindist=100000;
+            		int minx1=0;  
+            		int miny1=0;
+            		int minx2=0;
