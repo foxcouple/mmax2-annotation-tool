37c097b1c67cfafea1d5a01e708688749e64a245
foxcouple <134084166+foxcouple@users.noreply.github.com>
2022-07-02 05:53:59 +0000

    Update serveredit.php

--- Libs/MMAX2/org/eml/MMAX2/annotation/markables/SimpleMarkableCustomization.java
+++ Libs/MMAX2/org/eml/MMAX2/annotation/markables/SimpleMarkableCustomization.java
@@ -38,3 +38,198 @@ public class SimpleMarkableCustomization
     private MarkableLevel level = null;
     
     // Create two lists to store relations and assigned colors for relation-attributes
+    private ArrayList attributeNamesForColors = new ArrayList();
+    private ArrayList relationColors = new ArrayList();
+
+    // Create two lists to store relations and assigned line styles for relation-attributes
+    private ArrayList attributeNamesForStyles = new ArrayList();
+    private ArrayList relationStyles = new ArrayList();
+    
+    /** Creates new SimpleMarkableCustomization */
+    public SimpleMarkableCustomization(MarkableLevel _level, String raw_criteria, int _connector, SimpleAttributeSet _attributes) 
+    {
+        // Set numerical representation for connector
+        // The connector determines how several patterns in one rule be connected
+        connector = _connector;
+        // Set attributes to be returned upon match
+        attributes = _attributes;
+        // Set reference to markable level, needed to determine attribute type
+        level = _level;
+        // Create list to temporarily accept matching criterion objects
+        ArrayList temp = new ArrayList();
+                       
+        boolean negated = false;
+        boolean regExpMatch = false;
+        boolean matchAll = false;
+                
+//        boolean numMatch = false;
+        
+        // Get names of all style-attributes to be applied in style-part of this customization
+        Enumeration enum2 = attributes.getAttributeNames();
+        // Iterate over them
+        while (enum2.hasMoreElements())
+        {
+            // Get next style-attribute name
+            String tempElem = enum2.nextElement().toString();
+            if (tempElem.startsWith("markable_set_line_color") || tempElem.startsWith("markable_pointer_line_color"))
+            {
+                // Get attribute name the appearance of which is to be modified by this
+                String attribute = tempElem.substring(tempElem.indexOf(":")+1);
+                // Store name in list of attributes
+                attributeNamesForColors.add(attribute);
+                // Store Color to be applied in relationColors list
+                relationColors.add((Color)attributes.getAttribute(tempElem));
+            }
+            
+            else if (tempElem.startsWith("markable_set_line_style") || tempElem.startsWith("markable_pointer_line_style"))
+            {
+                // Get attribute name the appearance of which is to be modified by this
+                String attribute = tempElem.substring(tempElem.indexOf(":")+1);
+                // Store name in list of attributes
+                attributeNamesForStyles.add(attribute);
+                // Store style to be applied in relationColors list
+                relationStyles.add((Integer)attributes.getAttribute(tempElem));
+            }            
+        }
+        
+        // Split entire pattern line in individual patterns, separated by ;
+        StringTokenizer toki = new StringTokenizer(raw_criteria,";");
+        while (toki.hasMoreTokens())
+        {
+            // Get next token, i.e. next part of pattern element
+            String current = toki.nextToken().trim();
+//            if (current.startsWith("#"))
+//            {
+//                // The criterion starts with a #, so set to nummatch and cut off
+//                numMatch = true;
+//                current = current.substring(1).trim();
+//            }
+            
+            if (current.startsWith("!"))
+            {
+                // The criterion starts with a !, so negate and cut off
+                negated = true;
+                current = current.substring(1).trim();
+            }
+            if (current.startsWith("*"))
+            {
+                // The criterion starts with a *, so store as regExp and cut off
+                if (negated)
+                {
+                    System.out.println("Error: negated regExpMatch!");
+                    return;                    
+                }
+                regExpMatch = true;
+                current = current.substring(1).trim();
+            }
+            
+            String attributeName="";
+            String valString ="";
+            if (current.equalsIgnoreCase("{all}"))
+            {
+                matchAll= true;
+            }
+            else
+            {            
+                // Extract attribute name
+//                String attributeName = "";
+                try
+                {   
+                    attributeName = current.substring(0,current.indexOf("=")).trim();
+                }
+                catch (java.lang.StringIndexOutOfBoundsException ex)
+                {
+                    System.out.println("Error: Expected 'attribute name=value'");                
+                }
+                // extract String of target values
+                try
+                {
+                    valString = current.substring(current.indexOf("=")+1).trim();
+                }
+                catch (java.lang.StringIndexOutOfBoundsException ex)
+                {
+                    System.out.println("Error: Expected 'attribute name=value'");                
+                }
+                        
+                if (valString.startsWith("{")== false || valString.endsWith("}")== false)
+                {
+                    System.out.println("Error with customization value: "+valString);
+                    return;
+                }
+                valString = valString.substring(1,valString.length()-1).trim();
+            }
+            MMAX2MatchingCriterion crit = new MMAX2MatchingCriterion(attributeName, level.getCurrentAnnotationScheme().getAttributeTypeByAttributeName(attributeName), negated, regExpMatch);
+            
+            if (matchAll)
+            {
+                crit.setMatchAll();
+            }
+            else
+            {            
+                StringTokenizer valdi = new StringTokenizer(valString,",");
+                while(valdi.hasMoreTokens())
+                {            
+                    String val = (String)valdi.nextToken();
+                    crit.addValue(val);
+                }            
+            
+                if (crit.getSize()==0)
+                {
+                    crit.addValue("");
+                }
+            }
+            temp.add(crit);
+            crit = null;
+        }        
+        matchingCriteria = (MMAX2MatchingCriterion[]) temp.toArray(new MMAX2MatchingCriterion[temp.size()]);
+    }
+
+    
+    public final SimpleAttributeSet matches(Markable markable)
+    {            
+        boolean match = false;
+        SimpleAttributeSet result = null;
+        MMAX2MatchingCriterion currentCrit = null;
+        if (connector==MMAX2Constants.AND)
+        {
+            for (int z=0;z<matchingCriteria.length;z++)
+            {
+                currentCrit = matchingCriteria[z];
+                if (currentCrit.getNegated() && currentCrit.getRegExpMatch()==false)
+                {
+                    if (MarkableHelper.matchesAll(markable, currentCrit)) 
+                    {
+                        match = true;               
+                    }
+                    else
+                    {
+                        match = false;
+                        break;
+                    }
+                }
+                else
+                {
+                    if (MarkableHelper.matchesAny(markable, currentCrit)) 
+                    {
+                        match = true;               
+                    }
+                    else
+                    {
+                        match = false;
+                        break;
+                    }                    
+                }
+            }
+        }
+        else if (connector==MMAX2Constants.OR)
+        {
+            for (int z=0;z<matchingCriteria.length;z++)
+            {
+                currentCrit = matchingCriteria[z];
+                if (currentCrit.getNegated() && currentCrit.getRegExpMatch()==false)
+                {
+                    if (MarkableHelper.matchesAll(markable, matchingCriteria[z])) 
+                    {
+                        match = true;
+                        break;
+                    }
