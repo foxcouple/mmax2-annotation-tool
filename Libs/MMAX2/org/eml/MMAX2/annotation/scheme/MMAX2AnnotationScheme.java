d50196b354fcf1234d13b822589d6b6c1c9a5b70
foxcouple <134084166+foxcouple@users.noreply.github.com>
2022-07-02 17:15:05 +0000

    Update benchmark

--- Libs/MMAX2/org/eml/MMAX2/annotation/scheme/MMAX2AnnotationScheme.java
+++ Libs/MMAX2/org/eml/MMAX2/annotation/scheme/MMAX2AnnotationScheme.java
@@ -138,3 +138,214 @@ public class MMAX2AnnotationScheme implements AnnotationSchemeAPI
         /* Get all level element nodes from scheme file dom */
         NodeList allAttributeNodes = schemeDOM.getElementsByTagName("attribute");        
         
+        MMAX2Attribute currentAttribute = null;
+        Node currentNode = null;
+        
+        // Determine max. attribute name length in entire AnnotationScheme
+        for (int z=0;z<allAttributeNodes.getLength();z++)
+        {            
+            currentNode = allAttributeNodes.item(z);
+            String currentNodeName = currentNode.getAttributes().getNamedItem("name").getNodeValue();
+            if (currentNodeName.length() >= labelLength) labelLength = currentNodeName.length();
+        }       
+        
+        // This is for *attribute-level* tool tips and hints
+        String toolTipText = "";
+        for (int z=0;z<allAttributeNodes.getLength();z++)
+        {
+            currentNode = allAttributeNodes.item(z);
+            try											{ toolTipText = currentNode.getAttributes().getNamedItem("text").getNodeValue(); }
+            catch (java.lang.NullPointerException ex) 	{ toolTipText = "";}
+            
+            String descriptionFileName = "";
+            try 										{descriptionFileName = currentNode.getAttributes().getNamedItem("description").getNodeValue();}
+            catch (java.lang.NullPointerException ex)	{ }
+            
+            String hintText="";
+            if (descriptionFileName.equals("")==false) {hintText = readHTMLFromFile(schemeFileName.substring(0,schemeFileName.lastIndexOf(File.separator)+1)+descriptionFileName);}            
+            
+            String type = "";
+            int typeInt = -1;
+            try 										{ type = currentNode.getAttributes().getNamedItem("type").getNodeValue().trim(); }
+            catch (java.lang.NullPointerException ex)	{ type = "nominal_list";/* Use type 'nominal_list' as default */}
+                       
+            String attributeToShowInPointerFlag="";
+            type=type.toLowerCase();            
+            if (type.equals("nominal_button")) typeInt = AttributeAPI.NOMINAL_BUTTON;
+            else if (type.equals("nominal_list")) typeInt = AttributeAPI.NOMINAL_LIST;
+            else if (type.equals("freetext")) typeInt = AttributeAPI.FREETEXT;
+            else if (type.equals("markable_set")) typeInt = AttributeAPI.MARKABLE_SET;
+            else if (type.equals("markable_pointer")) typeInt = AttributeAPI.MARKABLE_POINTER;                        
+            else if (type.startsWith("markable_pointer:"))
+            {
+            	// Todo: Support more / other flags here
+                typeInt = AttributeAPI.MARKABLE_POINTER;
+                attributeToShowInPointerFlag=type.substring(type.indexOf(":")+1);
+            }
+            
+            int lineWidth = 2;
+            try								{ lineWidth = Integer.parseInt(currentNode.getAttributes().getNamedItem("width").getNodeValue()); }
+            catch (java.lang.Exception ex)	{ }
+            
+            Color color = Color.black;
+            try								{ color = MMAX2Utils.getColorByName((String)currentNode.getAttributes().getNamedItem("color").getNodeValue()); }
+            catch (java.lang.Exception ex)	{ }
+
+//            int alpha = 0;
+//            try								{ alpha =Integer.parseInt(currentNode.getAttributes().getNamedItem("alpha").getNodeValue()); }
+//            catch (java.lang.Exception ex)	{ }
+//
+//            if (alpha >0)
+//            {
+//                color = new Color(color.getRed(),color.getGreen(),color.getBlue(),alpha);
+//            }
+            
+            String lineStyle="straight";
+            int lineStyleInt=MMAX2Constants.STRAIGHT;
+            try								{ lineStyle = (String) currentNode.getAttributes().getNamedItem("style").getNodeValue().toLowerCase(); }
+            catch (java.lang.Exception ex)	{ }
+            
+            if      (lineStyle.equals("straight")) 		{ lineStyleInt = MMAX2Constants.STRAIGHT; }
+            else if (lineStyle.equals("lcurve")) 		{ lineStyleInt = MMAX2Constants.LCURVE; }
+            else if (lineStyle.equals("rcurve")) 		{ lineStyleInt = MMAX2Constants.RCURVE; }
+            else if (lineStyle.equals("xcurve")) 		{ lineStyleInt = MMAX2Constants.XCURVE; }
+            else if (lineStyle.equals("smartcurve"))	{ lineStyleInt = MMAX2Constants.SMARTCURVE; }
+            
+            // maxSize is the max. number of targets for a pointer attribute
+            int maxSize = -1;
+            try 							{ maxSize = Integer.parseInt(currentNode.getAttributes().getNamedItem("max_size").getNodeValue()); }
+            catch (java.lang.Exception ex)	{ }
+            
+            String targetDomain = "";
+            try 							{ targetDomain = (String) currentNode.getAttributes().getNamedItem("target_domain").getNodeValue(); }
+            catch (java.lang.Exception ex)  { }
+
+            boolean dashed=false;
+            String dashAttrib;
+            try								{ dashAttrib = (String) currentNode.getAttributes().getNamedItem("dashed").getNodeValue(); }
+            catch (java.lang.Exception ex)	{ dashAttrib = "false"; }
+            
+            if (dashAttrib.equalsIgnoreCase("true")) { dashed=true; }
+            
+            String add_to_markableset_instruction = "ADD TO MARKABLE SET";
+            try 							{ add_to_markableset_instruction = (String) currentNode.getAttributes().getNamedItem("add_to_markableset_text").getNodeValue(); }
+            catch (java.lang.Exception ex)	{ }
+
+            String remove_from_markableset_instruction = "REMOVE FROM MARKABLE SET";
+            try								{ remove_from_markableset_instruction = (String) currentNode.getAttributes().getNamedItem("remove_from_markableset_text").getNodeValue(); }
+            catch (java.lang.Exception ex)	{ }
+
+            String adopt_into_markableset_instruction = "ADOPT INTO MARKABLE SET";
+            try 							{ adopt_into_markableset_instruction = (String) currentNode.getAttributes().getNamedItem("adopt_into_markableset_text").getNodeValue(); }
+            catch (java.lang.Exception ex)	{ }
+            
+            String merge_into_markableset_instruction = "MERGE INTO MARKABLE SET";
+            try								{ merge_into_markableset_instruction = (String) currentNode.getAttributes().getNamedItem("merge_into_markableset_text").getNodeValue(); }
+            catch (java.lang.Exception ex)	{ }
+            
+            String point_to_markable_instruction = "POINT TO MARKABLE";
+            try								{ point_to_markable_instruction = (String) currentNode.getAttributes().getNamedItem("point_to_markable_text").getNodeValue(); }
+            catch (java.lang.Exception ex) 	{ }
+
+            String remove_pointer_to_markable_instruction = "REMOVE POINTER TO MARKABLE";
+            try								{ remove_pointer_to_markable_instruction = (String) currentNode.getAttributes().getNamedItem("remove_pointer_to_markable_text").getNodeValue(); }
+            catch (java.lang.Exception ex)	{ }
+            
+            UIMATypeMapping currentAttributeUIMAMapping = null;           
+            if (currentNode.getAttributes().getNamedItem("uima_type_mapping") != null) 	{ currentAttributeUIMAMapping = new UIMATypeMapping(currentNode.getAttributes().getNamedItem("uima_type_mapping").getNodeValue()); }
+            else 																		{ currentAttributeUIMAMapping = new UIMATypeMapping("",""); }                       
+
+            
+            /* Generate one MMAX2Attribute object for each attribute read from scheme file dom*/
+            /* name for this attribute is taken directly from scheme file DOM, id is set to lower-case to be robust when using 'next' attributes.*/            
+            currentAttribute = new MMAX2Attribute(	currentNode.getAttributes().getNamedItem("id").getNodeValue().toLowerCase(),
+            										currentNode.getAttributes().getNamedItem("name").getNodeValue(),
+            										typeInt,currentNode.getChildNodes(), this, 
+            										labelLength, toolTipText, hintText, lineWidth, color, lineStyleInt, 
+            										maxSize, targetDomain, 
+            										add_to_markableset_instruction, remove_from_markableset_instruction, 
+            										adopt_into_markableset_instruction, merge_into_markableset_instruction, 
+            										point_to_markable_instruction, remove_pointer_to_markable_instruction,
+            										fontSize, dashed, attributeToShowInPointerFlag, currentAttributeUIMAMapping);
+            
+            /* Map current attribute to its ID, which is sure to be lower-cased  */            
+            attributesByID.put(currentAttribute.getID(), currentAttribute);
+            
+            /* Map current attribute to its (lower-cased) attribute name */
+            /* LC is correct here because the name is used as hash key only. */
+            attributesByLowerCasedAttributeName.put(currentNode.getAttributes().getNamedItem("name").getNodeValue().toLowerCase(),currentAttribute);
+            attributes.add(currentAttribute);
+            currentAttribute = null;
+            size++;
+        }
+                
+        for (int z=0;z<attributes.size();z++)
+        {
+            currentAttribute = (MMAX2Attribute) attributes.get(z);           
+            MMAX2Attribute[] allDependentAttributes = currentAttribute.getDirectlyDependentAttributes();
+            for (int b=0;b<allDependentAttributes.length;b++)
+            {
+                allDependentAttributes[b].addDependsOn(currentAttribute);
+            }
+        }        
+        
+        attributepanel = new MMAX2AttributePanel(this);
+        attributepanel.create();            
+    }
+         
+    public boolean isVerbose()
+    {
+    	return VERBOSE;
+    }
+
+    public boolean isDebug()
+    {
+    	return DEBUG;
+    }
+    
+    /* This returns the unmodified name if no attribute of that name exists. */ 
+    public String normalizeAttributeName(String name) {
+    	//System.err.println("Java "+name.toLowerCase());
+    	if (attributesByLowerCasedAttributeName.containsKey(name.toLowerCase()))
+    	{
+    		// System.err.println("Found "+name.toLowerCase());
+    		return ((MMAX2Attribute)attributesByLowerCasedAttributeName.get(name.toLowerCase())).getDisplayName();
+    	}
+    	//else System.err.println("Not found "+name.toLowerCase());
+    		
+    	return name;
+    }
+	    
+    public UIMATypeMapping[] getAllUIMAAttributeMappings()
+    {
+    	ArrayList<UIMATypeMapping> tempResult = new ArrayList<UIMATypeMapping>();
+    	// Iterate over all attributes defined for this scheme (i.e. level)
+    	for (int z=0;z<attributes.size();z++)
+    	{
+    		MMAX2Attribute currentAttribute = (MMAX2Attribute) attributes.get(z);
+    		if (currentAttribute.getUIMATypeMapping()!=null)
+    		{
+    			tempResult.add(currentAttribute.getUIMATypeMapping());
+    		}
+    	}    	
+    	return (UIMATypeMapping[])tempResult.toArray(new UIMATypeMapping[0]);
+    }
+    
+    public static String readHTMLFromFile(String file)
+    {
+        String result="";
+        BufferedReader abbrevReader = null;
+        FileInputStream inStream = null;
+        try
+        {                
+            inStream = new FileInputStream(file);
+            abbrevReader = new BufferedReader(new InputStreamReader(inStream));//,"windows-1252"));
+        }
+        catch (java.io.FileNotFoundException ex)
+        {
+            System.err.println("Error: Couldn't find file "+file);
+            return "File "+file+" could not be found!";
+        }           
+        String currentLine="";
+        try
+        {
