3ae5f07200f07825b5efc6dab4037543f42878fc
foxcouple <134084166+foxcouple@users.noreply.github.com>
2023-02-15 09:23:59 +0000

    Update indexold.html

--- Libs/MMAX2/org/eml/MMAX2/gui/windows/MMAX2MarkablePointerBrowser.java
+++ Libs/MMAX2/org/eml/MMAX2/gui/windows/MMAX2MarkablePointerBrowser.java
@@ -71,3 +71,185 @@ public class MMAX2MarkablePointerBrowser extends javax.swing.JFrame implements j
         levelBox.setFont(MMAX2.getStandardFont());
         levelBox.addItem("<none>");
         MarkableLevel[] levels = discourse.getCurrentMarkableChart().getMarkableLevels();
+        for (int b=0;b<levels.length;b++)
+        {
+            levelBox.addItem(levels[b].getMarkableLevelName());
+        }
+        levelBox.addActionListener(this);
+        
+        Box tempLabelBox = Box.createVerticalBox();
+        Box tempControlBox = Box.createVerticalBox();
+        JMenuBar menu = new JMenuBar();
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        levelBoxLabel = new JLabel("Level:");
+        levelBoxLabel.setFont(MMAX2.getStandardFont());
+//        Box tempBox = Box.createVerticalBox();
+        tempLabelBox.add(levelBoxLabel);   
+        tempLabelBox.add(Box.createVerticalStrut(10));
+        tempControlBox.add(levelBox);
+        
+        attributeBox = new JComboBox<String>();
+        attributeBox.setFont(MMAX2.getStandardFont());
+        attributeBox.addItem("<none>");
+        attributeBox.addActionListener(this);
+        attributeBoxLabel = new JLabel("Attribute:");
+        attributeBoxLabel.setFont(MMAX2.getStandardFont());
+        tempLabelBox.add(attributeBoxLabel);        
+        tempLabelBox.add(Box.createVerticalStrut(20));
+        tempControlBox.add(attributeBox);
+        
+        permanentBox = new JCheckBox("Permanent");
+        permanentBox.setFont(MMAX2.getStandardFont());
+        permanentBox.addActionListener(this);
+        permanentBox.setEnabled(false);
+        tempControlBox.add(permanentBox);
+        panel.add(tempLabelBox);
+        panel.add(tempControlBox);        
+        
+        menu.add(panel);
+        setJMenuBar(menu);
+        setTitle("MMAX2 Markable Pointer Browser ["+mmax2.registerMarkablePointerBrowser(this)+"]");
+        setVisible(true);
+        pack();
+    }    
+    
+    public final void update()
+    {
+        updatePointerDisplay((String)levelBox.getSelectedItem(),(String)attributeBox.getSelectedItem());
+    }
+    
+    public final String getCurrentlyDisplayedMarkableLevelName()
+    {
+        return (String) levelBox.getSelectedItem();
+    }
+
+    public final String getCurrentlyDisplayedAttributeName()
+    {
+        return (String) attributeBox.getSelectedItem();
+    }
+    
+    
+    public final void updatePointerDisplay(String levelName, String attributeName)
+    {
+        // This method is called when some modification in the attributeBox occurred.
+        // It receives the name of the current level and the name of the attribute that
+        // was selected on that level, and updates the displayed tree to reflect
+        // all pointers belonging to that attribute.                
+        
+        HashSet<String> expanded = new HashSet<String>();
+        int setCount = root.getChildCount();
+        for (int z=0;z<setCount;z++)
+        {            
+            DefaultMutableTreeNode node = (DefaultMutableTreeNode)root.getChildAt(z);            
+            if (tree.isExpanded(new TreePath(node.getPath())))
+            {
+                expanded.add(((MarkablePointer)node.getUserObject()).getSourceMarkable().getID());
+            }
+        }
+        root.removeAllChildren();
+        
+        if (attributeName.equals("<none>")==false)
+        {                           
+            // Get the level of name levelName
+            MarkableLevel level = discourse.getCurrentMarkableChart().getMarkableLevelByName(levelName,false);
+            // Get the attribute on that level with name attributeName
+            MMAX2Attribute attribute = level.getCurrentAnnotationScheme().getUniqueAttributeByName("^"+attributeName.toLowerCase()+"$");
+            // Get the relation associated with that attribute
+            MarkableRelation relation =attribute.getMarkableRelation();
+            MarkablePointer[] pointers = relation.getMarkablePointers(true);
+            MarkablePointer currentPointer = null;
+            for (int z=0;z<pointers.length;z++)
+            {
+                currentPointer = pointers[z];
+                if (permanentBox.isSelected())
+                {
+                    currentPointer.setIsPermanent(true);
+                    mmax2.putOnRenderingList(currentPointer);
+                    //mmax2.redraw(null);
+                }
+                else
+                {
+                    mmax2.removeFromRenderingList(currentPointer);
+                }
+                
+                DefaultMutableTreeNode pointerNode = new DefaultMutableTreeNode();
+                pointerNode.setUserObject(currentPointer);
+                root.add(pointerNode);               
+                ArrayList<Markable> allTargets = new ArrayList<Markable>(java.util.Arrays.asList(currentPointer.getTargetMarkables()));
+                for (int b=0;b<allTargets.size();b++)
+                {
+                    DefaultMutableTreeNode elementNode = new DefaultMutableTreeNode(((Markable)allTargets.get(b)).toString());
+                    elementNode.setUserObject((Markable)allTargets.get(b));
+                    pointerNode.add(elementNode);
+                }
+                
+                if (expanded.contains(((MarkablePointer)pointerNode.getUserObject()).getSourceMarkable().getID()))
+                {
+                    tree.expandPath(new TreePath(pointerNode.getPath()));                    
+                }                
+            }
+        }
+        tree.updateUI();       
+        repaint();        
+        if (mmax2.getIsRendering())
+        {
+            mmax2.setRedrawAllOnNextRefresh(true);
+            mmax2.getCurrentTextPane().startAutoRefresh();
+        }
+        mmax2.redraw(null);        
+    }
+    
+    public final void updateAttributeList(String levelName)
+    {
+        // This method is called when some modification in the levelBox occurred.
+        // It receives the name of the level that was selected, and updates
+        // attributeBox so that it contains the names of all markable_set attributes
+        // on this level.
+        
+        attributeBox.removeActionListener(this);
+        // Clear attributeBox in any case
+        attributeBox.removeAllItems();
+        attributeBox.addItem("<none>");
+        
+        if (levelName.equals("<none>")==false)
+        {           
+            MarkableLevel level = discourse.getCurrentMarkableChart().getMarkableLevelByName(levelName, false);
+            MMAX2Attribute[] attribs = level.getCurrentAnnotationScheme().getAttributesByType(AttributeAPI.MARKABLE_POINTER);
+            for (int z=0;z<attribs.length;z++)
+            {
+                attributeBox.addItem(attribs[z].getDisplayName());
+            }
+        }
+        else
+        {
+            permanentBox.setEnabled(false);
+        }
+        attributeBox.addActionListener(this);
+        
+        root.removeAllChildren();
+        tree.updateUI();       
+        repaint();     
+        
+        if (attributeBox.getItemCount()==2)
+        {
+            attributeBox.setSelectedIndex(1);
+        }
+        pack();
+    }
+   
+    public final void resetPermanentDisplay()
+    {
+        int setCount = root.getChildCount();
+        for (int z=0;z<setCount;z++)
+        {            
+            DefaultMutableTreeNode node = (DefaultMutableTreeNode)root.getChildAt(z);            
+            mmax2.removeFromRenderingList(((MarkablePointer)node.getUserObject()));
+        }                        
+    }
+    
+    public final void dismiss()
+    {
+    	resetPermanentDisplay();
+        mmax2.unregisterMarkablePointerBrowser(this);
+        dispose();
