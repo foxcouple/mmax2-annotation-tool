cfafa1233663eaa03033bef457ea5e506ae3dc7b
foxcouple <134084166+foxcouple@users.noreply.github.com>
2023-02-15 17:58:53 +0000

    added issue template

--- Libs/MMAX2/org/eml/MMAX2/gui/windows/MMAX2PopupWindow.java
+++ Libs/MMAX2/org/eml/MMAX2/gui/windows/MMAX2PopupWindow.java
@@ -2,4 +2,10 @@
  * Copyright 2021 Mark-Christoph Müller
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance wit
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * dis
