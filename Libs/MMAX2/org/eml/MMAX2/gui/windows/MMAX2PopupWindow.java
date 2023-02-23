517199ac59b12aa32c622a5f28638d711da77e8b
foxcouple <134084166+foxcouple@users.noreply.github.com>
2023-02-23 12:37:50 +0000

    Formatting fixes.

--- Libs/MMAX2/org/eml/MMAX2/gui/windows/MMAX2PopupWindow.java
+++ Libs/MMAX2/org/eml/MMAX2/gui/windows/MMAX2PopupWindow.java
@@ -10,4 +10,10 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific langu
+ * See the License for the specific language governing permissions and
+ * limitations under the License. */
+
+package org.eml.MMAX2.gui.windows;
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.awt.imag
