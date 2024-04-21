5c8afa478314a16bf389afcbdb313cc477e7bcbb
foxcouple <134084166+foxcouple@users.noreply.github.com>
2024-04-21 08:12:29 +0000

    Add bower.json.

--- mmax2_flex.sh
+++ mmax2_flex.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-# libs="Libs/annotationdiffplugin.jar:jar_archive/1.14.
+# libs="Libs/annotationdiffplugin.jar:jar_archive/1.14.005/MMAX2.jar:Libs/batik-1.5.1/batik-awt-util.jar:Libs/batik-1.5.1/batik-dom.jar:Libs/batik-1.5.1/batik-svggen.jar:Libs/batik-1.5.1/batik-util.jar:Libs/batik-1.5.1/batik-xml.jar:Li
