40e81abd251af70cc9f8e3256ccbdfce5b44d228
foxcouple <134084166+foxcouple@users.noreply.github.com>
2024-04-30 11:32:19 +0000

    more and more

--- mmax2_flex.sh
+++ mmax2_flex.sh
@@ -3,4 +3,12 @@
 libs="Libs/annotationdiffplugin.jar:Libs/MMAX2.jar:Libs/batik-1.5.1/batik-awt-util.jar:Libs/batik-1.5.1/batik-dom.jar:Libs/batik-1.5.1/batik-svggen.jar:Libs/batik-1.5.1/batik-util.jar:Libs/batik-1.5.1/batik-xml.jar:Libs/jakarta-oro-2.0.8/jakarta-oro-2.0.8.jar:Libs/xalan-j_2_7_2-bin/xalan.jar:Libs/xalan-j_2_7_2-bin/xml-apis.jar:Libs/Xerces-J-bin.2.12.1/xercesImpl.jar:Libs/Xerces-J-bin.2.12.1/xml-apis.jar:Libs/Xerces-J-bin.2.12.1/serializer.jar:."
 
 if [ $# -eq 0 ]; then
-	java -Dfreetext_field_columns=20 -Dfreetext_font_increase=2  -classpath  $
+	java -Dfreetext_field_columns=20 -Dfreetext_font_increase=2  -classpath  $libs org.eml.MMAX2.core.MMAX2
+fi
+
+if [ $# -eq 1 ]; then
+	echo "Using default common_paths.xml"
+	java -Dfreetext_field_columns=20 -Dfreetext_font_increase=2 -classpath  $libs org.eml.MMAX2.core.MMAX2 $1
+fi
+
+if [ $# -eq
